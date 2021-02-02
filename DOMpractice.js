55b473887fb41791e4105a991ab3e012537c8623
Leily <leilymrz@gmail.com>
2021-02-02 07:31:40 -0800

    adding JS exercise 51

--- DOMpractice.js
+++ DOMpractice.js
@@ -1,6 +1,10 @@
-// ========Coding Exercise 52: querySelector Practice//======== -->
-// const doneTodos = document.querySelectorAll('.done');
-// const checkbox = document.querySelector('input[type="checkbox"]');
+// ========Coding Exercise 51: getElementById Practice
+const image = document.getElementById('unicorn');
+const heading = document.getElementById('mainheading');
+
+// ========Coding Exercise 52: querySelector Practice
+const doneTodos = document.querySelectorAll('.done');
+const checkbox = document.querySelector('input[type="checkbox"]');
 
 // >>>>>>>>Coding Exercise 53: Pickles Exercise<<<<<<<<<<<<<<<<<<<<<<
 const pickles = document.querySelector('span').innerText = 'Disgusting';

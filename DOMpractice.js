198f191c6b08e025bb5f1f8e704b78f58b8b5610
Leily <leilymrz@gmail.com>
2021-02-02 07:25:48 -0800

    Coding exercises 51, 52, 53

---
+++ DOMpractice.js
@@ -0,0 +1,6 @@
+// ========Coding Exercise 52: querySelector Practice//======== -->
+// const doneTodos = document.querySelectorAll('.done');
+// const checkbox = document.querySelector('input[type="checkbox"]');
+
+// >>>>>>>>Coding Exercise 53: Pickles Exercise<<<<<<<<<<<<<<<<<<<<<<
+const pickles = document.querySelector('span').innerText = 'Disgusting';

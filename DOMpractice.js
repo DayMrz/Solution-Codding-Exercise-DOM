e365da3d57e99694fb58185a4058de330ca79a72
Leily <leilymrz@gmail.com>
2021-02-02 21:00:26 -0800

    Adding exercise 54

--- DOMpractice.js
+++ DOMpractice.js
@@ -6,5 +6,13 @@ const heading = document.getElementById('mainheading');
 const doneTodos = document.querySelectorAll('.done');
 const checkbox = document.querySelector('input[type="checkbox"]');
 
-// >>>>>>>>Coding Exercise 53: Pickles Exercise<<<<<<<<<<<<<<<<<<<<<<
+// >>>>>>>>Coding Exercise 53: Pickles Exercise
 const pickles = document.querySelector('span').innerText = 'Disgusting';
+
+
+// >>>>>>>>Coding Exercise 54: Manipulating Attributes Practice
+const egg = document.querySelector('#egg')
+egg.getAttribute('src')
+egg.setAttribute('src', 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg')
+document.querySelector('#egg').alt = 'chicken'
+
